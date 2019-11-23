39e57519812b7738b4458f71563e2bc0a3c4097f
somanathan subramaniyan <36174210+SomanathanSubramaniyan@users.noreply.github.com>
2019-11-23 10:39:00 +0000

    Add files via upload

---
+++ Application.py
@@ -0,0 +1,15 @@
+#Lab - week 05
+from flask import Flask
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='../')
+
+@app.route('/')
+def index():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
