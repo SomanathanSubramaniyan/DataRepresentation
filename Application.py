6ff491e9fa5ba960056e33adee3c455949dbf91d
somanathan subramaniyan <36174210+SomanathanSubramaniyan@users.noreply.github.com>
2019-11-23 10:49:51 +0000

    Update Application.py

--- Application.py
+++ Application.py
@@ -1,9 +1,8 @@
 #Lab - week 05
 from flask import Flask
 
-app = Flask(__name__,
-            static_url_path='',
-            static_folder='../')
+app = Flask(__name__,static_url_path='',static_folder='.')
+
 
 @app.route('/')
 def index():
