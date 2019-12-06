c400de37cf2cb7db11eaaa04fea20e367d5782de
somanathan subramaniyan <36174210+SomanathanSubramaniyan@users.noreply.github.com>
2019-12-06 13:09:07 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='',static_folder='.')
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
     
